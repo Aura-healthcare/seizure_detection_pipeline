bd1c567b82a3d14a573ea293e47485b5a3fd7eae
Alexis COMTE <alexis.g.comte@gmail.com>
2021-10-27 19:01:16 +0200

    advancement of seizure detection pipeline DAG // not operational

--- dags/seizure_detection_pipeline.py
+++ dags/seizure_detection_pipeline.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from typing import Tuple
+import pandas as pd
 
 from airflow.decorators import dag, task
-
+import sys
+sys.path.append('.')
 from src.usecase.fetch_database import fetch_database
 from src.usecase.detect_qrs import detect_qrs
 from src.usecase.apply_ecg_qc import apply_ecg_qc
@@ -14,17 +16,17 @@ CONCURRENCY = 4
 SCHEDULE_INTERVAL = None
 DEFAULT_ARGS = {'owner': 'airflow'}
 
-DICT_PARAMS_EDF_FILE = {
-    "patient": "PAT_6",
-    "record": "77",
-    "file_path": "",
-    "segment": "s1",
-    "channel_name": "emg6+emg6-",
-    "start_time": "2020-12-18 13:00:00",
-    "end_time": "2020-12-18 14:30:00",
-    "data_path": 'data'
-}
-INFOS = list(DICT_PARAMS_EDF_FILE.values())[:4]
+# DICT_PARAMS_EDF_FILE = {
+#     "patient": "PAT_6",
+#     "record": "77",
+#     "file_path": "",
+#     "segment": "s1",
+#     "channel_name": "emg6+emg6-",
+#     "start_time": "2020-12-18 13:00:00",
+#     "end_time": "2020-12-18 14:30:00",
+#     "data_path": 'data'
+# }
+# INFOS = list(DICT_PARAMS_EDF_FILE.values())[:4]
 
 DETECT_QRS_METHOD = 'hamilton'
 ECG_QC_MODEL = 'rfc_normalized_2s.pkl'
@@ -39,23 +41,30 @@ LENGTH_CHUNK = 2
      concurrency=CONCURRENCY)
 def dag_seizure_detection_pipeline():
 
-
-    @task(multiple_outputs=True)
-    def t_detect_qrs(dict_params: dict) -> Tuple[int, str]:
-        sampling_frequency, filename = detect_qrs(**dict_params)
-        return {
-            "sampling_frequency": sampling_frequency,
-            "filename": filename
-        }
-
     @task(multiple_outputs=True)
     def t_fetch_database(dict_params: dict) -> Tuple[str, str]:
         db_list_filename = fetch_database(**dict_params)
         return db_list_filename
 
+#     @task(multiple_outputs=True)
+#     def t_read_fetched_database(db_list_filename: str) -> pd.DataFrame:
+#         df_db = pd.read_csv(db_list_filename, encoding="utf-8"),
+#         df_db_length = df_db.shape[0]
+#         return df_db, df_db_length
+# 
     @task()
-    def t_apply_ecg_qc(dict_params: dict) -> str:
-        filename = apply_ecg_qc(**dict_params)
+    def t_detect_qrs(dict_params: dict) -> Tuple[int, str]:
+        sampling_frequency, file_rr_intervals = detect_qrs(**dict_params)
+        return file_rr_intervals#, sampling_frequency
+
+    @task()
+    def t_apply_ecg_qc(filename: str,
+                       sampling_frequency: int,
+                       model: str,
+                       exam_id: str) -> str:
+
+        filename = apply_ecg_qc(filename, sampling_frequency, model, exam_id)
+
         return filename
 
     @task()
@@ -74,48 +83,38 @@ def dag_seizure_detection_pipeline():
         }
     )
 
+    # df_db, df_db_length = t_read_fetched_database(db_list_filename)
+    df_db = pd.read_csv("output/db/df_data.csv", encoding="utf-8")
 
-    data_list_filename = db_list_filename["data"]
-    annotation_list_filename = db_list_filename["annotations"]
+    for index in range(df_db.shape[0]):
+        data_file_path = df_db['data_file_path'].iloc[index]
+        exam_id = df_db['exam_id'].iloc[index]
+        file_rr_intervals = t_detect_qrs(
+            {
+                "filename": data_file_path,
+                "exam_id": exam_id,
+                "method": DETECT_QRS_METHOD
+            })
 
-    print(db_list_filename)
-    print(data_list_filename)
-    #db = pd.read_csv("output/db/df_data.csv", encoding="utf-8")
+        file_quality = t_apply_ecg_qc(filename=data_file_path,
+                                       sampling_frequency=1000,
+                                       model=ECG_QC_MODEL,
+                                       exam_id=exam_id)
 
-    #return_dict = t_detect_qrs(
-    #    {
-    #        "filename": exam["data_file_path"],
-    #        "exam_id": exam["exam_id"],
-    #        "method": DETECT_QRS_METHOD
-    #    }
-    #)
-
-    #sampling_frequency = return_dict["sampling_frequency"]
-    #file_rr_intervals = return_dict["filename"]
-    #
-    #file_quality = t_apply_ecg_qc(
-    #    {
-    #        **DICT_PARAMS_EDF_FILE,
-    #        "model": ECG_QC_MODEL,
-    #        "infos": INFOS
-    #    }
-    #)
-    #
-    #file_clean_rr_intervals = t_remove_noisy_segment(
-    #    {
-    #        "rr_intervals_file": file_rr_intervals,
-    #        "chunk_file": file_quality,
-    #        "length_chunk": LENGTH_CHUNK,
-    #        "sampling_frequency": sampling_frequency
-    #    }
-    #)
-    #
-    #t_ecg_qc_statistical_analysis(
-    #    {
-    #        "chunk_file": file_quality
-    #    }
-    #
-    #)
-
+        file_clean_rr_intervals = t_remove_noisy_segment(
+            {
+                "rr_intervals_file": file_rr_intervals,
+                "chunk_file": file_quality,
+                "length_chunk": LENGTH_CHUNK,
+                "sampling_frequency": 1000 
+            }
+        )
+        
+        t_ecg_qc_statistical_analysis(
+            {
+                "chunk_file": file_quality
+            }
+        
+        )
 
 dag_pipeline = dag_seizure_detection_pipeline()
