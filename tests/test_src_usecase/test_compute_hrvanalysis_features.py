18eb6ed1e4179e956f3116a53bcfa9a8757c7415
Alexis COMTE <alexis.g.comte@gmail.com>
2021-10-27 19:02:23 +0200

    adding unit testing on hrvanalysis

--- tests/test_src_usecase/test_compute_hrvanalysis_features.py
+++ tests/test_src_usecase/test_compute_hrvanalysis_features.py
@@ -2,14 +2,15 @@ import pandas as pd
 
 from src.usecase.compute_hrvanalysis_features import compute_hrvanalysis_features
 
+RR_INTERVAL_FILEPATH = 'tests/test_data/00009578_s002_t001.csv'
 
 def test_compute_hrvanalysis_features_should_return_dataframe():
     # Given
     # When
-    result = compute_hrvanalysis_features()
+    result = compute_hrvanalysis_features(RR_INTERVAL_FILEPATH)
 
     # Then
-    assert isinstance(result, pd.DataFrame)
+    assert(type(result) == str)
 
 
 def test_compute_hrvanalysis_features_should_return_a_dataframe_with_proper_shape():
