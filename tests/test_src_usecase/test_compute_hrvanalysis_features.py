3c9f0c0f2bd93d8ab6d33caf2ac1412ff396e73c
Alexis COMTE <alexis.g.comte@gmail.com>
2021-11-03 15:03:10 +0100

    updating test for compute_hrv_analysis_features

--- tests/test_src_usecase/test_compute_hrvanalysis_features.py
+++ tests/test_src_usecase/test_compute_hrvanalysis_features.py
@@ -2,16 +2,13 @@ import pandas as pd
 
 from src.usecase.compute_hrvanalysis_features import compute_hrvanalysis_features
 
-RR_INTERVAL_FILEPATH = 'tests/test_data/00009578_s002_t001.csv'
+OUTPUT_FOLDER = 'tests/output/features'
 
 def test_compute_hrvanalysis_features_should_return_dataframe():
-    # Given
-    # When
-    # result = compute_hrvanalysis_features(RR_INTERVAL_FILEPATH)
-    # Need for refactoring with output folder
-    # Then
-    # assert(type(result) == str)
-    pass
+    rr_interval_file_path = 'tests/test_data/00009578_s002_t001.csv'
+    result = compute_hrvanalysis_features(rr_intervals_file_path=rr_interval_file_path,
+                                          output_folder=OUTPUT_FOLDER)
+    assert(type(result) == str)
 
 
 def test_compute_hrvanalysis_features_should_return_a_dataframe_with_proper_shape():
