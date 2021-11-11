7974e9ce7a73eab33782a89e9af45564706a35c8
Alexis COMTE <alexis.g.comte@gmail.com>
2021-11-11 17:14:38 +0100

    commented qrs detection script and related unit tests

--- src/usecase/detect_qrs.py
+++ src/usecase/detect_qrs.py
@@ -1,29 +1,45 @@
 import os
 import argparse
-from typing import Tuple, List
+from typing import List
 import sys
 
 sys.path.append('.')
-
+from src.usecase.utilities import convert_args_to_dict, generate_output_path
 from src.domain.qrs_detector import QRSDetector
 from src.infrastructure.edf_loader import EdfLoader
-from src.usecase.utilities import convert_args_to_dict
 
 OUTPUT_FOLDER = 'output/rr_intervals'
 METHODS = ['hamilton', 'xqrs', 'gqrs', 'swt', 'engelsee']
 DEFAULT_METHOD = 'hamilton'
 
 
-def detect_qrs(filepath: str,
+def detect_qrs(qrs_file_path: str,
                method: str,
                exam_id: str,
-               output_folder: str = OUTPUT_FOLDER) -> Tuple[int, str]:
-    '''
+               output_folder: str = OUTPUT_FOLDER) -> str:
+    """
     Detects QRS on a signal, and writes their frame and RR-intervals in a csv
     file.
-    '''
-    # Read ECG channel from EDF files
-    edfloader = EdfLoader(filepath)
+
+    parameters
+    ----------
+    qrs_file_path : str
+        The path of the file including ECG signal
+    method : str
+        QRS detection method, to be chose between hamilton, xqrs, gqrs, swt
+        and engelsee
+    exam_id : str
+        ID of the exam to load
+    output_folder : str
+        Path of the output folder
+
+    returns
+    -------
+    output_file_path : str
+        Path where detected qrs are stored in csv format
+    """
+    # Reads ECG channel from EDF files
+    edfloader = EdfLoader(qrs_file_path)
     ecg_channel_name = edfloader.get_ecg_candidate_channel()
     start_time, end_time = edfloader.get_edf_file_interval()
 
@@ -43,19 +59,34 @@ def detect_qrs(filepath: str,
     df_detections.drop(columns='signal', inplace=True)
 
     # Export
-    os.makedirs(output_folder, exist_ok=True)
-    export_filename = f"{exam_id}.csv"
-    export_filepath = os.path.join(output_folder, export_filename)
-    df_detections.to_csv(export_filepath, sep=',', index=True)
+    output_file_path = generate_output_path(
+        input_file_path=exam_id,
+        output_folder=output_folder,
+        format='csv')
 
-    return sampling_frequency, export_filepath
+    df_detections.to_csv(output_file_path, sep=',', index=True)
+
+    return output_file_path, sampling_frequency
 
 
 def parse_detect_qrs_args(args_to_parse: List[str]) -> argparse.Namespace:
+    """
+    Parse arguments for adaptable input.
 
+    parameters
+    ----------
+    args_to_parse : List[str]
+        List of the element to parse. Should be sys.argv[1:] if args are
+        inputed via CLI
+
+    returns
+    -------
+    args : argparse.Namespace
+        Parsed arguments
+    """
     parser = argparse.ArgumentParser(description='CLI parameter input')
-    parser.add_argument('--filepath',
-                        dest='filepath',
+    parser.add_argument('--qrs-file-path',
+                        dest='qrs_file_path',
                         required=True)
     parser.add_argument('--method',
                         dest='method',
@@ -71,9 +102,21 @@ def parse_detect_qrs_args(args_to_parse: List[str]) -> argparse.Namespace:
     return args
 
 
-def parse_exam_id(filepath: str) -> str:
+def parse_exam_id(qrs_file_path: str) -> str:
+    """
+    From a qrs_file_path including an exam ID, return the ID directly.
 
-    exam_id = os.path.basename(filepath)
+    parameters
+    ----------
+    qrs_file_path : str
+        The path to the qrs file
+
+    returns
+    -------
+    exam_id : str
+        The parsed exam id
+    """
+    exam_id = os.path.basename(qrs_file_path)
 
     return exam_id
 
@@ -83,6 +126,6 @@ if __name__ == "__main__":
     args = parse_detect_qrs_args(sys.argv[1:])
     args_dict = convert_args_to_dict(args)
     if 'exam_id' not in args_dict:
-        exam_id = parse_exam_id(args_dict['filepath'])
+        exam_id = parse_exam_id(args_dict['qrs_file_path'])
         args_dict.update({'exam_id': exam_id})
     detect_qrs(**args_dict)
--- tests/test_src_usecase/test_detect_qrs.py
+++ tests/test_src_usecase/test_detect_qrs.py
@@ -1,5 +1,6 @@
 import argparse
-from src.usecase.detect_qrs import detect_qrs, parse_detect_qrs_args
+from src.usecase.detect_qrs import detect_qrs, parse_detect_qrs_args,\
+    parse_exam_id
 from src.usecase.utilities import convert_args_to_dict
 TEST_TUH_EDF_FILENAME = \
     'data/tuh/dev/01_tcp_ar/002/00009578/00009578_s002_t001.edf'
@@ -12,23 +13,24 @@ TEST_METHODS = ['hamilton', 'xqrs', 'gqrs', 'swt', 'engelsee']
 def test_tuh_detect_qrs():
 
     for method in TEST_METHODS:
-        sampling_frequency, export_filename = detect_qrs(
-            filepath=TEST_TUH_EDF_FILENAME,
+        export_filename, sampling_frequency = detect_qrs(
+            qrs_file_path=TEST_TUH_EDF_FILENAME,
             method=method,
             exam_id=TEST_TUH_EXAM_ID,
             output_folder=OUTPUT_FOLDER)
 
         assert(sampling_frequency == 250)
-        assert (export_filename == ''.join([OUTPUT_FOLDER,
-                                     '/',
-                                     TEST_TUH_EXAM_ID,
-                                     '.csv']))
+        assert (export_filename == ''.join(
+            [OUTPUT_FOLDER,
+             '/',
+             TEST_TUH_EXAM_ID,
+             '.csv']))
 
 
 def test_tuh_parse_detect_qrs_args():
 
     bash_command = (f'python3 src/usecase/detect_qrs.py '
-                    f'--filepath {TEST_TUH_EDF_FILENAME} '
+                    f'--qrs-file-path {TEST_TUH_EDF_FILENAME} '
                     f'--method hamilton '
                     f'--exam-id {TEST_TUH_EXAM_ID} '
                     f'--output-folder {OUTPUT_FOLDER}')
@@ -37,10 +39,15 @@ def test_tuh_parse_detect_qrs_args():
     parser_dict = convert_args_to_dict(parser)
 
     correct_parser = argparse.Namespace(
-        exam_id=TEST_TUH_EXAM_ID,
-        filepath=TEST_TUH_EDF_FILENAME,
+        qrs_file_path=TEST_TUH_EDF_FILENAME,
         method='hamilton',
+        exam_id=TEST_TUH_EXAM_ID,
         output_folder=OUTPUT_FOLDER)
     correct_parser_dict = convert_args_to_dict(correct_parser)
 
     assert(parser_dict == correct_parser_dict)
+
+
+def test_parse_exam_id():
+    exam_id = parse_exam_id(qrs_file_path=TEST_TUH_EDF_FILENAME)
+    assert(exam_id == '00009578_s002_t001.edf')
