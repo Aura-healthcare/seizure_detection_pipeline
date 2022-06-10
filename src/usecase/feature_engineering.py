ecfb6519a90c204b6e12c7d6e486c5df7a23a00b
infini11 <sndm1995@gmail.com>
2022-06-10 17:14:42 +0200

    Some refactoring about feature engineering briks

--- src/usecase/feature_engineering.py
+++ src/usecase/feature_engineering.py
@@ -7,107 +7,128 @@ some pca analysis to reduce multi-colinéarité.
 """
 
 import string
+from typing import Tuple
 
 import numpy as np
+from numpy import ndarray
 import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.impute import SimpleImputer
 from sklearn.decomposition import PCA
 from scipy.stats import zscore
 
-class FeatureEngineering:
+
+def get_dataset(dataset_path: str, col_to_drop: list) -> Tuple(pd.DataFrame, pd.DataFrame):
+
+    """
+    This function will get the dataset by using path. It will also do some
+    preparation about dataset before feature engineering.
+    """
     
-    def __init__(
-        self,
-        patient_dataset_path: string,
-        objectif_variance: float
-    ) -> None:
+    dataframe: pd.DataFrame = pd.read_csv(dataset_path)
+    dataframe_copy: pd.DataFrame = dataframe.copy()
 
-        #read file in dataframe from path patient dataset
-        self.dataframe: pd.DataFrame = pd.read_csv(patient_dataset_path)
+    dataframe_copy = dataframe_copy.sort_values(by='timestamp').reset_index(drop=True)
+    dataframe_copy.drop(col_to_drop, axis=1, inplace=True)
 
-        self.dataframe_copy: pd.DataFrame = self.dataframe.copy()
+    return dataframe, dataframe_copy
 
-        #sort row by timestamp
-        self.dataframe_copy = self.dataframe_copy.sort_values(by = 'timestamp').reset_index(drop=True)
 
-        #drop some columns that will be unused
-        #self.dataframe_copy.drop(['filename', 'interval_index', 'timestamp', 'set'], axis=1, inplace=True)
-
-    def impute_nan_and_infinite_values(self):
-
-        """
-        This function will replace infinite values by nan. 
-        Because of many nan values can follow one another in an entire window, 
-        we choose to impute nan values by global mean in order to avoid extrems values.
-        """
-        if np.isinf(self.dataframe_copy).sum().any() > 0:
-            self.dataframe_copy.replace([np.inf, -np.inf], np.nan, inplace=True)
-
-        X = self.dataframe_copy.drop(['label'], axis=1)
-        Y = self.dataframe_copy['label']
-        X_imputed = None
-
-        if self.dataframe_copy.isna().sum().any() :
-            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-            imputer.fit(X)
-            X_imputed = imputer.transform(X)
-
-        return X_imputed, Y
-
-    def outlier_detection(self, X):
-
-        """
-        This function detect outlier based on local density of points. It will identify
-        point with low local density compared with their neighboors and remove them.
-        """
-
-        # Detection of outliers
-        LOF = LocalOutlierFactor()
-        Y_pred = LOF.fit_predict(X)
-        X_score = LOF.negative_outlier_factor_
-
-        outlier_score = pd.DataFrame()
-        outlier_score['score'] = X_score
-
-        X_init = self.dataframe_copy.drop(['label'], 1)
-        Y_init = self.dataframe_copy['label']
-        dataframe = pd.DataFrame(X, columns=X_init.columns)
-
-        ## drop outliers values
-        filt = outlier_score["score"] < -1.5
-        outlier_index = outlier_score[filt].index.tolist()
-        new_X = pd.DataFrame(dataframe).drop(outlier_index)
-        new_Y = Y_init.drop(outlier_index).values
-
-        return new_X, new_Y
-
+def replace_infinite_values_by_nan(dataframe: pd.DataFrame) -> pd.DataFrame:
     
-    def pca_analysis(self, dataframe):
+    """
+    This is a simple function that replace infinite values by nan.
+    """
 
-        """
-        This function resolve the problem of multicolinearity in dataset. Some features are too
-        correlated. This can be redundant information that we need to remove.
-        """
-        #normalisation step
-        dataframe = zscore(dataframe)
+    dataframe.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-
-        pca = PCA(n_components=self.objectif_variance)
-        pca_out = pca.fit(dataframe)
-        principalComponents = pca.transform(dataframe)
-
-        #construction of dataset
-        new_size_dataset = pca_out.components_.shape[0]
-        list_colmuns = []
-        for i in range(1, new_size_dataset):
-            list_colmuns.append("PC"+i)
-        
-        pca_df = pd.DataFrame(principalComponents, columns=list_colmuns)
-        pca_df['label'] = self.Y
-
-        return pca_df
+    return dataframe
 
 
-        
+def impute_nan_values_by_median(dataframe: pd.DataFrame) -> Tuple(np.array, pd.DataFrame):
+    
+    """
+    This function will replace nan values by median. 
+    Because of many nan values can follow one another in an entire window, 
+    we choose to impute nan values by global median in order to avoid extrems values.
+    """
+
+    X = dataframe.drop(['label'], axis=1)
+    Y = dataframe['label']
+
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    X_imputed = imputer.fit_transform(X)
+    
+    return X_imputed, Y
+
+
+def outlier_detection(X: np.array) -> pd.DataFrame:
+
+    """
+    This function detect outlier based on local density of points.
+    """
+
+    LOF = LocalOutlierFactor()
+    Y_pred = LOF.fit_predict(X)
+    X_score = LOF.negative_outlier_factor_
+
+    outlier_score = pd.DataFrame()
+    outlier_score['score'] = X_score
+
+    return outlier_score
+
+
+def remove_outlier(
+    X_imputed_df: pd.DataFrame,
+    outlier_score: pd.DataFrame,
+    Y: pd.DataFrame
+    ) -> Tuple(pd.DataFrame, pd.DataFrame):
+
+    """
+    This function will remove outlier on the dataset
+    """
+
+    filt = outlier_score["score"] < -1.5
+    outlier_index = outlier_score[filt].index.tolist()
+    X_result_outlier = pd.DataFrame(X_imputed_df).drop(outlier_index)
+    Y_result_outlier = Y.drop(outlier_index).values
+
+    return X_result_outlier, Y_result_outlier
+
+
+def pca_analysis(dataframe: pd.DataFrame, Y: pd.DataFrame, objective_variance) -> Tuple(any, np.ndarray):
+
+    """
+    This function resolve the problem of multicolinearity in dataset. Some features are too
+    correlated. This can be redundant information that we need to remove.
+    """
+    #normalisation step
+    dataframe = zscore(dataframe)
+
+
+    pca = PCA(n_components=objective_variance)
+    pca_out = pca.fit(dataframe)
+    principalComponents = pca.transform(dataframe)
+
+
+    return pca_out, principalComponents
+
+
+def create_final_dataset_with_pca(pca_out, principalComponents: np.ndarray, Y: pd.DataFrame) -> pd.DataFrame:
+
+    """
+    This function will create the final dataset by adding label.
+    """
+
+    #construction of dataset
+    new_size_dataset = pca_out.components_.shape[0]
+    list_colmuns = []
+    for i in range(1, new_size_dataset):
+        list_colmuns.append("PC"+i)
+    
+    pca_df = pd.DataFrame(principalComponents, columns=list_colmuns)
+
+    pca_df['label'] = Y
+
+    return pca_df
 
