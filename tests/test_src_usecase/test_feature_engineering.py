eb817ba7ae9ddf1e8e80cef35610c4fbeb5ee80d
infini11 <sndm1995@gmail.com>
2022-07-06 14:40:38 +0200

    REFACT: Feature engineerning

--- tests/test_src_usecase/test_feature_engineering.py
+++ tests/test_src_usecase/test_feature_engineering.py
@@ -18,9 +18,14 @@ from src.usecase.feature_engineering import (
                                                 impute_nan_values_by_median,
                                                 outlier_detection,
                                                 pca_analysis,
+                                                perform_op_on_features,
+                                                prepare_features,
+                                                prepare_features_with_io,
                                                 replace_infinite_values_by_nan,
                                                 remove_outlier,
-                                                createContextualFeatues
+                                                computeContextFeatures,
+                                                computeSeasonerFeatures,
+                                                diffOperationFromFeatures
                                             )
 
 def test_get_dataset(dataframe):
@@ -66,7 +71,7 @@ def test_impute_values_by_median_when_inf_values_exist(dataframe):
 
 def test_impute_values_by_median_when_inf_values_not_exist(dataframe):
     # Given
-    expected_response = True
+    expected_response = False
     col_to_drop = ['timestamp', 'set']
 
     # When
@@ -76,7 +81,7 @@ def test_impute_values_by_median_when_inf_values_not_exist(dataframe):
     dataframe_imputed = pd.DataFrame(X_imputed)
 
     # Then
-    assert dataframe_imputed.isna().values.any() != expected_response
+    assert dataframe_imputed.isna().values.any() == expected_response
     assert dataframe_imputed.shape[0] == y.shape[0]
 
 
@@ -115,6 +120,7 @@ def test_remove_outlier():
     # Given
     dataset_path = DATASET_FILE_PATH
     col_to_drop = ['timestamp', 'set']
+    threshold = -1.5
 
     # When
     _, dataframe = get_dataset(dataset_path, col_to_drop)
@@ -126,7 +132,8 @@ def test_remove_outlier():
     X_result_outlier, Y_result_outlier = remove_outlier(
         dataframe_imputed,
         outlier_score,
-        y
+        y,
+        threshold
     )
 
     # Then
@@ -191,16 +198,149 @@ def test_remove_outlier():
 #     # assert pca_df['label'].shape[0] == Y_result_outlier.shape[0]
 
 
-def test_createContextualFeatures():
+def test_computeSeasonerFeatures_when_list_time_is_given():
     # Given
-    expected_response = False
     dataset_path = DATASET_FILE_PATH_FEAT
     col_to_drop = []
-    _ , dataframe = get_dataset(dataset_path, col_to_drop)
+    list_time = ["dayOfWeek", "month", "hour", "minute"]
 
     # When
-    dataframe_with_contextual_feat = createContextualFeatues(dataframe)
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    dataframe_with_seasonal_informations = computeSeasonerFeatures(
+                                                dataframe=dataframe,
+                                                list_time=list_time
+                                            )
 
     # Then
-    assert dataframe_with_contextual_feat.empty == expected_response
-    assert len(dataframe.columns.to_list()) < len(dataframe_with_contextual_feat.columns.to_list())
+    assert "dayOfWeek" in dataframe_with_seasonal_informations.keys()
+    assert "month" in dataframe_with_seasonal_informations.keys()
+    assert "hour" in dataframe_with_seasonal_informations.keys()
+    assert "minute" in dataframe_with_seasonal_informations.keys()
+
+
+def test_perform_op_on_features():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_feat = ["mean_hr"]
+    operation = "std"
+    list_period = [30, 60]
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    df_with_operation_result = perform_op_on_features(
+                                dataframe=dataframe.copy(),
+                                list_feat=list_feat,
+                                list_period=list_period,
+                                operation=operation
+                            )
+    # Then
+    assert "mean_hr_30" in df_with_operation_result.keys()
+    assert "mean_hr_30" not in dataframe.keys()
+    
+
+def test_computeSeasonerFeatures_when_list_time_not_given():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_time = []
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    dataframe_with_seasonal_informations = computeSeasonerFeatures(
+                                                dataframe=dataframe,
+                                                list_time=list_time
+                                            )
+
+    # Then
+    assert_frame_equal(dataframe, dataframe_with_seasonal_informations)
+
+def test_computeContextFeatures_when_list_feat_is_given():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_feat = ["mean_hr", "mean_nni"]
+    list_period = [30, 60]
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    df_with_contextual_feat_time = computeContextFeatures(
+                                    dataframe=dataframe.copy(),
+                                    operation="mean",
+                                    list_feat=list_feat,
+                                    list_period=list_period
+                                )
+    
+    # Then
+    assert "mean_hr_30" in df_with_contextual_feat_time.keys()
+    assert "mean_hr_60" in df_with_contextual_feat_time.keys()
+    assert "mean_nni_30" in df_with_contextual_feat_time.keys()
+    assert "mean_nni_60" in df_with_contextual_feat_time.keys()
+    assert len(dataframe.columns.to_list()) < len(df_with_contextual_feat_time.columns.to_list())
+
+
+def test_computeContextFeatures_when_list_feat_not_given():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_feat = []
+    list_period = [30, 60]
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    df_with_contextual_feat_time = computeContextFeatures(
+                                    dataframe=dataframe.copy(),
+                                    operation="mean",
+                                    list_feat=list_feat,
+                                    list_period=list_period
+                                )
+    
+    # Then
+    assert_frame_equal(dataframe, df_with_contextual_feat_time)
+
+
+def test_diffOperationFromFeatures_when_list_feat_is_given():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_feat = ["mean_hr", "mean_nni"]
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    df_with_diff_features = diffOperationFromFeatures(dataframe=dataframe, list_feat=list_feat)
+
+    # Then
+    assert "mean_hr_diff" in df_with_diff_features.keys()
+    assert "mean_nni_diff" in df_with_diff_features.keys()
+
+
+def test_diffOperationFromFeatures_when_list_feat_not_given():
+    # Given
+    dataset_path = DATASET_FILE_PATH_FEAT
+    col_to_drop = []
+    list_feat = []
+
+    # When
+    _, dataframe = get_dataset(dataset_path, col_to_drop)
+    df_with_diff_features = diffOperationFromFeatures(dataframe=dataframe, list_feat=list_feat)
+
+    # Then
+    assert_frame_equal(dataframe, df_with_diff_features)
+
+
+def test_prepare_features_with_io():
+    # Given
+    expected_response = False
+    dataset_path = DATASET_FILE_PATH_FEAT,
+    col_to_drop= ['interval_index', 'interval_start_time', 'filename']
+    features_path = "data/test_data/ml.csv"
+
+    # When
+    prepare_features_with_io(
+        dataset_path=dataset_path,
+        col_to_drop=col_to_drop,
+        features_path=features_path)
+    res = pd.read_csv(features_path)
+
+    # Then
+    assert res.empty == expected_response
